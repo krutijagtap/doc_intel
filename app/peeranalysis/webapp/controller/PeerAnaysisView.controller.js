562dccefb26224e534eb6197e24cc9d790e4d77c
Dorian Li <dorian.li@sap.com>
2025-10-09 17:17:30 +0800

    upload6

--- app/peeranalysis/webapp/controller/PeerAnaysisView.controller.js
+++ app/peeranalysis/webapp/controller/PeerAnaysisView.controller.js
@@ -440,8 +440,8 @@ sap.ui.define([
       onUploadFileContent: async function (oFile) {
         const chatModel = this.getOwnerComponent().getModel("chatModel");
         chatModel.setProperty("/busyIndicator", true);
-        const url = this.getBaseURL() + "/v2/odata/v4/earning-upload-srv/uploadPromptFile";
-        // const url = this.getBaseURL() + "/api/chat_upload";
+        // const url = this.getBaseURL() + "/v2/odata/v4/earning-upload-srv/uploadPromptFile";
+        const url = this.getBaseURL() + "/api/chat_upload";
         const csrfUrl = this.getBaseURL() + "/v2/odata/v4/earning-upload-srv/";
         const csrf = await this.onfetchCSRF(csrfUrl);
        let formData = new FormData();
